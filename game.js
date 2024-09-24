3614f305dd0a9c855ebcf92ae67cb956c63c27f6
Thomas <163157813+Thomasbka@users.noreply.github.com>
2024-09-24 11:20:43 +0200

    Created function for getting getting the players guess. Also created a function which checks the guess, if it's too high, too low or correct.

--- game.js
+++ game.js
@@ -1,3 +1,25 @@
 const generateRandomNumber = function() {
   return Math.floor(Math.random() * 100) + 1;
 }
+
+const getPlayerGuess = function() {
+  const guess = Number(prompt("Enter a guess between 1 and 100: "));
+
+  if (guess >= 1 && guess <= 100) {
+    return guess;
+  } else {
+    alert("This input is not valid. Please enter a number between 1 and 100");
+    return getPlayerGuess();
+  }
+};
+
+const checkGuess = function(playerGuess, correctNumber) {
+  if (playerGuess < correctNumber) {
+    return "Your guess is too low! Try again..";
+  } else if (playerGuess > correctNumber) {
+    return "Your guess is too high! Try again..";
+  } else {
+    return "Your guess is correct!";
+  }
+};
+
